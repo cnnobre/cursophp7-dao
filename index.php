ed0ff06b69b5f3aa75d41da5f10763622c2af5fc
Cleilson Nobre <cleilson_nobre@hotmail.com>
2022-01-28 14:41:20 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadbyId(3);
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo"); 
+//echo json_encode($search); 
 
+//Carrega um usuário usando o login e a senha 
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
 
  ?>
